31845366ae5488d90af49d9ad4a6cbbf7a79ee6b
siya <siyakansagara@gmail.com>
2025-12-28 15:26:43 +0530

    Add files via upload

---
+++ lab 4/LibraryItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    class LibraryItem
+    {
+        protected string title;
+        protected string author;
+
+        public LibraryItem(string t, string a)
+        {
+            title = t;
+            author = a;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Title: {title}, Author: {author}");
+        }
+    }
+
+    class Book : LibraryItem
+    {
+        int pages;
+
+        public Book(string t, string a, int p) : base(t, a)
+        {
+            pages = p;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Book → Title: {title}, Author: {author}, Pages: {pages}");
+        }
+    }
+
+    class Magazine : LibraryItem
+    {
+        int issueNo;
+
+        public Magazine(string t, string a, int i) : base(t, a)
+        {
+            issueNo = i;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Magazine → Title: {title}, Author: {author}, Issue No: {issueNo}");
+        }
+    }
+}
---
+++ lab 4/Magazine.cs
@@ -0,0 +1,16 @@
+﻿namespace lab_4
+{
+    internal class Magazine : LibraryItem
+    {
+        private string mtit;
+        private string maut;
+        private int issue;
+
+        public Magazine(string mtit, string maut, int issue)
+        {
+            this.mtit = mtit;
+            this.maut = maut;
+            this.issue = issue;
+        }
+    }
+}
