edd4fce8982231d8161038665adc17ed15c29b66
siya <siyakansagara@gmail.com>
2025-12-28 15:27:54 +0530

    Add files via upload

---
+++ lab 4/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    class BankAccount
+    {
+        private string accountName;
+        private double balance;
+
+        public BankAccount(string name, double bal)
+        {
+            accountName = name;
+            balance = bal;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void UpdateBalance(double amount)
+        {
+            balance += amount;
+        }
+
+        public string GetName()
+        {
+            return accountName;
+        }
+    }
+
+    class BankTransaction
+    {
+        // Transfer without message
+        public void Transfer(BankAccount from, BankAccount to, double amount)
+        {
+            if (amount <= 0 || from.GetBalance() < amount)
+                throw new Exception("Invalid transfer amount");
+
+            from.UpdateBalance(-amount);
+            to.UpdateBalance(amount);
+
+            Console.WriteLine("Transfer Successful");
+        }
+
+        // Overloaded Transfer with message
+        public void Transfer(BankAccount from, BankAccount to, double amount, string message)
+        {
+            Transfer(from, to, amount);
+            Console.WriteLine("Message: " + message);
+        }
+    }
+}
---
+++ lab 4/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    class Customer
+    {
+        protected double amount;
+
+        public Customer(double amt)
+        {
+            amount = amt;
+        }
+
+        public virtual double CalculateBill()
+        {
+            return amount;
+        }
+    }
+
+    class RegularCustomer : Customer
+    {
+        public RegularCustomer(double amt) : base(amt) { }
+
+        public override double CalculateBill()
+        {
+            return amount - (amount * 0.05); // 5% discount
+        }
+    }
+
+    class PremiumCustomer : Customer
+    {
+        public PremiumCustomer(double amt) : base(amt) { }
+
+        public override double CalculateBill()
+        {
+            return amount - (amount * 0.15); // 15% discount
+        }
+    }
+}
